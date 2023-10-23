6c01e46456222d36f2188fe4618e1c9f75d5d36c
Marcos Machado Duarte <91387237+MarcosMDuarte@users.noreply.github.com>
2023-10-22 23:12:47 -0300

    versao final

--- src/main/java/duarte/marcos/SantanderBootcamp2023Application.java
+++ src/main/java/duarte/marcos/SantanderBootcamp2023Application.java
@@ -1,8 +1,11 @@
 package duarte.marcos;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "URL padrão")})
 @SpringBootApplication
 public class SantanderBootcamp2023Application {
 
